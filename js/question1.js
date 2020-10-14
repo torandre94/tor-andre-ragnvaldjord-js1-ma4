859a0892b38738f269da6f36707f8073a9ea4b38
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-10-14 12:49:40 +0200

    Update question1.js

--- js/question1.js
+++ js/question1.js
@@ -1,1 +1,16 @@
+const form = document.querySelector("#contactForm");
+const lastName = document.querySelector("#lastName");
+const lastNameError = document.querySelector("#lastNameError");
 
+function validateForm() {
+    event.preventDefault();
+
+    if (lastName.value.trim().length > 4) {
+        lastNameError.style.display = "none";
+    }
+    else {
+        lastNameError.style.display = "block";
+    }
+}
+
+form.addEventListener("submit", validateForm);
