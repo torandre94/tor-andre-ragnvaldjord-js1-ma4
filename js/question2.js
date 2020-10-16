47f1620c5b3a05ccbaf18eff6a81d4744391399e
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-10-16 20:24:22 +0200

    Update question2.js

--- js/question2.js
+++ js/question2.js
@@ -3,6 +3,8 @@ const resultsContainer = document.querySelector(".results");
 const url = "https://api.rawg.io/api/games?dates=1999-01-01,1999-12-31&ordering=-rating";
 
 async function callApi() {
+
+    try {
     const response = await fetch(url);
     const json = await response.json();
 
@@ -12,11 +14,15 @@ async function callApi() {
         resultsContainer.innerHTML +=   `<a href="question3.html?id=${result.id}">
                                             <h3>${result.name}</h3>
                                             <p>Rating: ${result.rating}</p>
-                                            <p>Released: ${result.released}</p>
+                                            <time>Released: ${result.released}</time>
                                             <img src="${result.background_image}" alt="${result.name}" />
                                         </a>`;
                                         
     });
+    }
+    catch(error) {
+        resultsContainer.innerHTML = "An error has occurred!"
+    }
 }
 
 callApi();
