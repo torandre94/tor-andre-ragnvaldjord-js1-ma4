7c4f6e8682d85250af79a864612b6ced2dee5d7d
torandre94 <61887464+torandre94@users.noreply.github.com>
2020-10-16 19:59:55 +0200

    Update question3.js

--- js/question3.js
+++ js/question3.js
@@ -1,1 +1,28 @@
+const detailContainer = document.querySelector(".detail");
 
+const queryString = document.location.search;
+const params = new URLSearchParams(queryString);
+const id = params.get("id");
+
+
+const url = "https://api.rawg.io/api/games/" + id;
+
+
+async function fetchGame() {
+
+        const response = await fetch(url);
+        const details = await response.json();
+
+        createHtml(details);
+    
+}
+
+fetchGame();
+
+function createHtml(details) {
+    detailContainer.innerHTML = `<h1>${details.name}</h1>
+                                <p>${details.description}</p>
+                                <p>Released: ${details.released}</p>
+                                <p>Rating: ${details.rating}</p>
+                                <p>Number of ratings: ${details.ratings_count}</p>`;
+}
